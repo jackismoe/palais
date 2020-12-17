b305c0b9060c274fe3c84d37f21e5a5501038048
Jack <jacksonmorgan@hey.com>
2020-12-17 15:45:52 -0500

    Set up initial fetch for db data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  // fake placeholder for state
+  // allRestaurants = []
+  // fastFoodRestaurants = []
+  // dineInRestaurants = []
+  // food = []
+
+  // fetchData() {
+  //   fetch(`http://localhost:3001/dineins`)
+  //   .then(response => response.json())
+  //   .then(jsonResponse => {
+  //     for (let restaurant of jsonResponse) {
+  //       this.dineInRestaurants.push(restaurant)
+  //       this.allRestaurants.push(restaurant)
+  //     }
+  //   })
+  //   fetch(`http://localhost:3001/fastfoods`)
+  //   .then(response => response.json())
+  //   .then(jsonResponse => {
+  //     for (let restaurant of jsonResponse) {
+  //       this.fastFoodRestaurants.push(restaurant)
+  //       this.allRestaurants.push(restaurant)
+  //     }
+  //   })
+  //   fetch(`http://localhost:3001/foods`)
+  //   .then(response => response.json())
+  //   .then(jsonResponse => {
+  //     for (let food of jsonResponse) {
+  //       this.food.push(food)
+  //     }
+  //   })  
+  // }
+
+  render() {
+    return(
+      <div>
+        <button>
+        does nothing
+        </button>
+      </div>
+    )
+  }
 }
-
-export default App;
